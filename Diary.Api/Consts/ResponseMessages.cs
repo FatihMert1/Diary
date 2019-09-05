37ff47fee94c882137f39e65869878e765890a5a
Fatih Rahman Mert <fatihmert3858@gmail.com>
2019-09-05 17:53:25 +0300

    Created Diary Controller

---
+++ Diary.Api/Consts/ResponseMessages.cs
@@ -0,0 +1,16 @@
+namespace Diary.Api.Consts
+{
+    public sealed  class ResponseMessages
+    {
+        public const string Success = "Successfully";
+        public const string Failed = "Failled";
+        public const string Wrong = "Something Went Wrong";
+        public const string Waiting = "The Process Waiting";
+        public const string InvalidContentType = "Invalid Content Type";
+        public const string InvalidRole = "Invalid Role";
+        public const string UnauthorizedAccess = "Unauthorized Access";
+        public const string AuthenticateFailed = "Authenticate Failled";
+        public const string AuthenticateSuccess = "Authenticate Successfully";
+        
+    }
+}
---
+++ Diary.Api/Controllers/DiaryController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Diary.Api.Controllers
+{
+    [ApiController]
+    [Route("diary")]
+    [Produces("application/json")]
+    [Consumes("application/json")]
+    public class DiaryController : Controller
+    {
+        
+    }
+}
